6c2bc505039d82d091e1742366cf2d8c1129c346
Lucas Vinícios <lvinicios365@gmail.com>
2025-06-15 18:25:39 -0300

    bootstrap corrigido

--- kanban-frontend/src/main.ts
+++ kanban-frontend/src/main.ts
@@ -1,13 +1,18 @@
-// main.ts
+// src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app'; // Corrigido para .component
+import { AppComponent } from './app/app';
 import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes'; // Importa suas rotas
-import { provideHttpClient } from '@angular/common/http'; // Importa provideHttpClient
+import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap'; // Importe NgbModal
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient() // Adicione esta linha para fornecer o HttpClient
+    provideHttpClient(),
+    // Remova NgbModule se estiver aqui
+    // NgbModule,
+    // E adicione a linha abaixo:
+    { provide: NgbModal, useClass: NgbModal } // Fornece NgbModal explicitamente
   ]
 }).catch(err => console.error(err));
